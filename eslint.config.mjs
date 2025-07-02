5949541a9a8cc5f7b6e93de0b6a6beda5b64043b
minji <swij1004@naver.com>
2025-07-02 15:22:43 +0900

    feat: lint options 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import importPlugin from 'eslint-plugin-import';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,80 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+
+  // 전체 파일
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    settings: {
+      react: { version: 'detect' },
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+    rules: {
+      eqeqeq: ['warn', 'smart'],
+      'no-unused-vars': ['warn', { args: 'none', ignoreRestSiblings: true }],
+      'prefer-const': 'warn',
+      'no-use-before-define': 'off',
+      'no-inner-declarations': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          pathGroups: [
+            {
+              pattern: '@mui/**',
+              group: 'external',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+        },
+      ],
+    },
+  },
+  // tsx
+  {
+    files: ['**/*.tsx'],
+    rules: {
+      // React
+      'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+      'react/forbid-foreign-prop-types': ['warn', { allowInPropTypes: true }],
+      'react/jsx-no-comment-textnodes': 'warn',
+      'react/jsx-no-duplicate-props': 'error',
+      'react/jsx-no-target-blank': 'error',
+      'react/jsx-no-undef': 'error',
+      'react/jsx-pascal-case': ['warn', { allowAllCaps: true, ignore: [] }],
+      'react/jsx-uses-vars': 'warn',
+      'react/jsx-handler-names': 'error',
+      'react/no-danger-with-children': 'warn',
+      'react/no-deprecated': 'warn',
+      'react/no-direct-mutation-state': 'warn',
+      'react/no-typos': 'error',
+      'react/style-prop-object': 'warn',
+      'react/self-closing-comp': 'error',
+
+      // Hooks
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
